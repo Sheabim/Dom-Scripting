1ebd6257273ec275cd147e153e68bcd162d7accf
shailabrazil <sheilaadesina85@gmail.com>
2018-10-29 15:14:46 +1100

    Change
    Js

--- js/app.js
+++ js/app.js
@@ -4,23 +4,47 @@ const ul = document.getElementById('invitedList');
 
 
 form.addEventListener ('submit', (e) =>{
-e.preventDefault();
-const text = input.value;
-input.value = " ";
-const ul = document.getElementById('invitedList');
-const li = document.createElement('li');
-li.textContent = text;
+      e.preventDefault();
+      //CRETING INPUT AND ADDING ON HTML
+      const text = input.value;
+      input.value = " ";
+      const ul = document.getElementById('invitedList'); //THIS BECAME SCOPE AFTER
+      const li = document.createElement('li');
+      li.textContent = text;//TYPE OF CONTENT
+      ul.appendChild(li)//ADDING ON HTML
 
-const label =  document.createElement('label');
-label.textContent = 'confirmed';
-li.appendChild(label);
- //CREATING CHECK BOXY AND APPENDING
-const checkbox = document.createElement('input')
-checkbox.type = 'checkbox';
- ul.appendChild(li)
- label.appendChild(checkbox);
- li.appendChild(label);
+      //CRATEING THE LABEL CONFIRM WILL BE ADD ON HTML
+      const label =  document.createElement('label');
+      label.textContent = 'confirmed';//TYPE OF CONTENT
+      li.appendChild(label);//ADDING ON HTML
+
+       //CREATING CHECK BOXY AND APPENDING
+      const checkbox = document.createElement('input')
+      checkbox.type = 'checkbox';//TYPE OF CONTENT
+       label.appendChild(checkbox);//ADDING ON HTML
+
 });
+
+
+ul.addEventListener('change', (e) => {
+       const checkbox = event.target;// event call
+       const checked = checkbox.checked; //check the variable
+       const listItem = checkbox.parentNode.parentNode;// i dont know
+
+
+       if (checked) {
+         listItem.clasName = 'responed';
+       } else {
+         listItem.className = '';
+
+        }
+
+
+});
+
+
+
+
 
 // const form = document.getElementById('registrar');
 // const input = document.querySelector('input');
